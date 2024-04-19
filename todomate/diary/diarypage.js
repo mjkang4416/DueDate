8f934d14442d0997eb296f2bc6f715ba94695b62
jejuorangee <idjjh1998@gmail.com>
2024-04-19 22:54:26 +0900

    diarypage css

--- todomate/diary/diarypage.js
+++ todomate/diary/diarypage.js
@@ -204,6 +204,29 @@ cancel.addEventListener("click", () => {
   writeDiaryForm.reset();
 });
 
+// mypage menu--------------------
+document.querySelector(".mypage-btn").addEventListener("click", function () {
+  document.getElementById("mypage-menu-wrap").classList.toggle("active-page");
+});
+
+document.querySelector(".close-btn").addEventListener("click", function () {
+  document.getElementById("mypage-menu-wrap").classList.remove("active-page");
+});
+
+document.addEventListener("click", function (event) {
+  var menu = document.getElementById("mypage-menu-wrap");
+  var target = event.target;
+  var isMenuButton =
+    target.matches(".mypage-btn") || target.closest(".mypage-btn");
+  var isMenu =
+    target.matches(".mypage-menu-wrap") || target.closest(".mypage-menu-wrap");
+  var isCloseButton = target.matches(".close-btn");
+
+  if (!isMenuButton && !isMenu && !isCloseButton) {
+    menu.classList.remove("active-page");
+  }
+});
+
 //-------------------------------------------------------- 작은 달력 -------------------------------------------------------------------
 
 // const leftCurrentDate = document.querySelector('.leftCurrent-date');
