1216af5d1531391433445f7c1f30b089bd823d8c
jejuorangee <idjjh1998@gmail.com>
2024-05-08 00:08:47 +0900

    diary

--- duedate/duedate/src/test/java/com/example/duedate/repository/DiaryDAOTest.java
+++ duedate/duedate/src/test/java/com/example/duedate/repository/DiaryDAOTest.java
@@ -1,4 +1,48 @@
+package com.example.duedate.repository;
+
+import com.example.duedate.domain.diarydto.DiaryDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Slf4j
 class DiaryDAOTest {
-  
+
+    @Autowired
+    DiaryDAO diaryDAO;
+
+    @Test
+    void testInsertDiary() {
+        DiaryDTO diaryDTO = new DiaryDTO();
+
+        diaryDTO.setDiaryDate("2024-05-05");
+        diaryDTO.setDiaryEmo("🍔");
+        diaryDTO.setDiaryTitle("hello");
+        diaryDTO.setDiaryContent("hi");
+
+        System.out.println(diaryDTO);
+
+
+//        diaryDAO.insertDiary(diaryDTO);
+    }
+
+    @Test
+    void updateDiary() {
+    }
+
+    @Test
+    void selectDiaryDetail() {
+    }
+
+    @Test
+    void deleteDiary() {
+    }
+
+    @Test
+    void selectEmoAll() {
+    }
 }
